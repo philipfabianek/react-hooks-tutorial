f144c247fb009d5f28b5df6e8b2770d58710fbfe
Philip <fabianekphilip@gmail.com>
2021-04-06 08:57:08 +0200

    Episode 3

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [stateValue, updateStateValue] = useState(1);
+
+  const [objectState, updateObjectState] = useState({
+    property1: "property 1",
+    property2: "property 2",
+  });
+
   return (
     <div className="App">
-      React hooks tutorial
+      {stateValue}
+
+      <button onClick={() => {
+        const newStateValue = stateValue + 1;
+        updateStateValue(newStateValue);
+      }}>
+        +
+      </button>
+      <button onClick={() => {
+        updateStateValue((previousStateValue) => {
+          return previousStateValue - 1;
+        });
+      }}>
+        -
+      </button>
+
+      <p>{objectState.property1}</p>
+      <p>{objectState.property2}</p>
+
+      <button onClick={() => {
+        updateObjectState((previousObjectState) => {
+          return {
+            ...previousObjectState,
+            property1: "Updated property 1",
+          };
+        });
+      }}>
+        Update property 1
+      </button>
     </div>
   );
 }
